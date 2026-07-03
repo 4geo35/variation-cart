37c56d436e84c8bbca813d75cb181aebcd9cad5d
Labrock <vladimirpeshkov5@yandex.ru>
2026-07-03 10:34:07 +0300

    add empty check

--- src/resources/views/livewire/web/catalog/cart-ico-wire.blade.php
+++ src/resources/views/livewire/web/catalog/cart-ico-wire.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-center">
     <a href="{{ route('web.cart') }}" class="inline-block hover:text-primary-hover relative">
-        @if ($cartInfo->count)
+        @if (! empty($cartInfo->count))
             <span class="inline-flex justify-center items-center bg-primary min-w-5 py-0.5 px-1.5 rounded-full text-white text-xs font-semibold absolute -top-2 -right-2 z-10">
                 {{ $cartInfo->count }}
             </span>
